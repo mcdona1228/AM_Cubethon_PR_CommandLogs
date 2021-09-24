6de58fd0098ddfe627feee68e9064d85485cfd7b
mcdona1228 <61477480+mcdona1228@users.noreply.github.com>
2021-09-24 13:50:50 -0700

    CommandLog

---
+++ GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+
+    public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel ()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+            Invoke("Restart", 2f);
+        }
+    }
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+   // Command command = CommandLog.commands.Peek();
+
+}
